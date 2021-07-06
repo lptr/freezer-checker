d8210cfbbb7e6064227a55e6d3054981101d9418
Lóránt Pintér <lorant.pinter@gmail.com>
2021-07-06 22:15:46 +0200

    Add WIFIManager

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,36 @@
 #include <Arduino.h>
 
-#include "DHT.h"
+#include <DHT.h>
+#include <WiFiManager.h>
 
 #define DHTPIN D1
-
 #define DHTTYPE DHT11
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
-// Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
     Serial.begin(115200);
 
     dht.begin();
+
+    WiFi.mode(WIFI_STA);
+
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+
+    //reset settings - wipe credentials for testing
+    //wifiManager.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    if (!wifiManager.autoConnect("freezer")) {
+        Serial.println("Failed to connect");
+    } else {
+        Serial.println("connected...yeey :)");
+    }
 }
 
 void loop() {
