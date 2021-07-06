f6d8e6db55a4cdd1af3698859be40dd3a8a4a07f
Lóránt Pintér <lorant.pinter@gmail.com>
2021-07-07 01:07:45 +0200

    Add delay config

--- src/main.cpp
+++ src/main.cpp
@@ -3,56 +3,53 @@
 #include <Adafruit_BME280.h>
 #include <Adafruit_Sensor.h>
 #include <ArduinoJson.h>
+#include <I2CScanner.h>
 #include <PubSubClient.h>
 #include <WiFiManager.h>
-#include <I2CScanner.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME280 bme;
 WiFiClient espClient;
-PubSubClient client(espClient);
+PubSubClient mqtt(espClient);
 
 unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE (4096)
 const char* mqtt_server = "10.180.1.194";
+long delayInMs = 5000;
 
 void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print("Message arrived [");
     Serial.print(topic);
     Serial.print("] ");
-    for (unsigned int i = 0; i < length; i++) {
-        Serial.print((char) payload[i]);
-    }
-    Serial.println();
-
-    // Switch on the LED if an 1 was received as first character
-    if ((char) payload[0] == '1') {
-        digitalWrite(LED_BUILTIN, LOW);    // Turn the LED on (Note that LOW is the voltage level
-                                           // but actually the LED is on; this is because
-                                           // it is active low on the ESP-01)
-    } else {
-        digitalWrite(LED_BUILTIN, HIGH);    // Turn the LED off by making the voltage HIGH
+    DynamicJsonDocument doc(1024 + length * 2);
+    deserializeJson(doc, (char*) payload, length);
+    String json;
+    serializeJsonPretty(doc, json);
+    Serial.println(json);
+    if (doc["delay"].is<long>()) {
+        delayInMs = doc["delay"];
+        Serial.printf("Updated delay to %ld\n", delayInMs);
     }
 }
 
-void reconnect() {
+void mqttReconnect() {
     // Loop until we're reconnected
-    while (!client.connected()) {
+    while (!mqtt.connected()) {
         Serial.print("Attempting MQTT connection...");
-        // Create a random client ID
+        // Create a random mqtt ID
         String clientId = "freezer-";
         clientId += String(random(0xffff), HEX);
         // Attempt to connect
-        if (client.connect(clientId.c_str())) {
+        if (mqtt.connect(clientId.c_str())) {
             Serial.println("connected");
             // Once connected, publish an announcement...
-            client.publish("freezer/connect", "{ \"connected\": true }");
+            mqtt.publish("freezer/connect", "{ \"connected\": true }");
             // ... and resubscribe
-            client.subscribe("freezer/config");
+            mqtt.subscribe("freezer/config");
         } else {
             Serial.print("failed, rc=");
-            Serial.print(client.state());
+            Serial.print(mqtt.state());
             Serial.println(" try again in 5 seconds");
             // Wait 5 seconds before retrying
             delay(5000);
@@ -96,16 +93,11 @@ void setup() {
 
     randomSeed(micros());
     pinMode(LED_BUILTIN, OUTPUT);    // Initialize the BUILTIN_LED pin as an output
-    client.setServer(mqtt_server, 1883);
-    client.setCallback(callback);
+    mqtt.setServer(mqtt_server, 1883);
+    mqtt.setCallback(callback);
 }
 
 void loop() {
-    if (!client.connected()) {
-        reconnect();
-    }
-    client.loop();
-
     float humidity = bme.readHumidity();
     float temperature = bme.readTemperature();
     float pressure = bme.readPressure();
@@ -121,7 +113,9 @@ void loop() {
     doc["altitude"] = altitude;
     serializeJson(doc, payload);
 
-    client.publish("freezer/telemetry", payload.c_str());
+    mqttReconnect();
+    mqtt.loop();
+    mqtt.publish("freezer/telemetry", payload.c_str());
 
-    delay(5000);
+    delay(delayInMs);
 }
